1955da9eb27cebee6ba0e9f6f9ac59bb2bf2d8f1
dodo838383 <51447247+dodo838383@users.noreply.github.com>
2022-10-03 20:59:15 -0300

    Primeira fase

--- streamlit_01.py
+++ streamlit_01.py
@@ -3,3 +3,9 @@ import numpy as np
 import seaborn as sns
 import streamlit as st
 import pickle
+
+with st.form ('Frame'):
+    collection = st.number_input('Digite a coleção de sua escolha:')
+    budget = st.number_input('Digite o valor do orçamento do filme:') 
+    Time_taken =  st.number_input('Digite a duração do filme:')
+    
